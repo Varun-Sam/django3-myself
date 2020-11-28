16ad8cbe7e4ca958937c694d63691d07271b9e3e
unknown <varunsam9900@gmail.com>
2020-11-28 14:11:14 +0530

    sixth commit

--- myself_site/settings.py
+++ myself_site/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tc9c03s-vap6t%(3^ix+tr^shw$=uwc+d$w7e9a0g@o*)z7m=r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['varunsam9900.pythonanywhere.com']
 
 # Application definition
 
